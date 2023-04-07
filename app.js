1417af6bc3bc2997a6bcdf7cab8c6a9ebc6287a3
Arvindh <echoesincharade@gmail.com>
2023-04-08 00:48:22 +0530

    Blog Schema

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
 app.listen(3001, ()=> {
     console.log("Server is running at port 3001");
 });
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+    {
+        userNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if(err) {
+            console.log(err);
+        } else{
+            console.log("Connected to MongoDB");
+        }
+    }
+);
 
 module.exports = app;
